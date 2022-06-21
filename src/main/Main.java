380044e3531739ffb51fd4a6a97b3e09d16bb7d2
martinrojasmet <martin.rojas@correo.unimet.edu.ve>
2022-06-20 20:39:31 -0400

    sout test

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,6 +23,7 @@ public class Main {
     public static void main(String[] args) {
         GlobalUI.openMainPage();
         FunctionsTXT.loadExistingArticlesInDB();
+        System.out.println("holis");
 //        testHash();
     }
 
