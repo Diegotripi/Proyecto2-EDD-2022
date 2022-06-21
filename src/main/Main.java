86af901c64735b5c172129df4a086973d2df2599
Diego Munoz De Escalona <77362303+Diegotripi@users.noreply.github.com>
2022-06-20 20:14:10 -0400

    prueba

--- src/main/Main.java
+++ src/main/Main.java
@@ -23,6 +23,8 @@ public class Main {
     public static void main(String[] args) {
         GlobalUI.openMainPage();
         FunctionsTXT.loadExistingArticlesInDB();
+        GlobalUI.getArticleHT().printAllArticlesConsole();
+        
 //        testHash();
     }
 
