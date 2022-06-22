daa842dc5ca6985420bc5cc01fdfafd7fd35182d
martinrojasa <martinrojasa@outlook.com>
2022-06-21 20:06:31 -0400

    analyze summary beta

---
+++ src/DataStructures/WordRepetition.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataStructures;
+
+/**
+ *
+ * @author marti
+ */
+public class WordRepetition {
+    //FALTA JAVADOC
+    String word;
+    int repetition;
+
+    public WordRepetition(String word) {
+        this.word = word;
+        this.repetition = 0;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String title) {
+        this.word = word;
+    }
+
+    public int getRepetition() {
+        return repetition;
+    }
+
+    public void setRepetition(int repetition) {
+        this.repetition = repetition;
+    }
+  
+}
--- src/UI/FunctionsUI.java
+++ src/UI/FunctionsUI.java
@@ -6,8 +6,14 @@
 package UI;
 
 import Classes.Article;
+import DataStructures.HashTable;
+import DataStructures.LinkedList;
+import DataStructures.Node;
+import DataStructures.WordRepetition;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
 import main.FunctionsTXT;
 
 /**
@@ -49,5 +55,99 @@ public class FunctionsUI {
             }
         }
     }
+    
+    //FALTA JAVADOC
+    public static void fillComboBoxWithListStrings(JComboBox<String> comboBox, LinkedList List) {
+        comboBox.removeAllItems();
+        int index = 0;
+        Node pointer = List.getHead();
+        while (pointer != null) {
+            comboBox.insertItemAt((String) pointer.getElement(), index);
+            index++;
+            pointer = pointer.getNext();
+        }
+    }
+    
+    //FALTA JAVADOC
+    public static void analyzeSummary(String key, JTextArea textArea) {
+        if (key != null) {
+            
+            String textResult = "Nombre del trabajo: " + key + "\n";
+            
+            LinkedList list = GlobalUI.getArticleHT().getTable()[hashStringUpperC(key, GlobalUI.getArticleHT())];
+            
+            Node pointer = list.getHead();
+            while (pointer != null && !((Article) pointer.getElement()).getTitle().equals(key)) {
+                pointer = pointer.getNext();
+            }
+            
+            textResult += "Autores: " + ((Article) pointer.getElement()).getAuthors().getAuthorsString() + "\n\n";               
+            String summary = ((Article) pointer.getElement()).getBody();
+            
+            String[] wordArray = summary.replaceAll("[,.;:'?!)(]", "").replace(" ","\n").split("\n");
+            String[] keyWordsArray = new String[((Article) pointer.getElement()).getKeyWords().getLength()];
+            HashTable wordsHT = new HashTable();
+            
+            int index2 = 0;
+            Node pointer5 = ((Article) pointer.getElement()).getKeyWords().getHead();
+            while (pointer5 != null) {
+                WordRepetition wordAndRepetition = new WordRepetition(((String) pointer5.getElement()));
+                keyWordsArray[index2] = (String) pointer5.getElement();
+                wordsHT.addWordInListOfRep(wordAndRepetition);
+                index2++;
+                pointer5 = pointer5.getNext();
+            }
+            
+            for (int i = 0; i < wordArray.length; i++) {
+                int index = FunctionsUI.hashStringUpperC(wordArray[i], wordsHT);
+                try {
+                    Node pointer1 = wordsHT.getTable()[index].getHead();
+                    while (pointer1 != null) {
+                        if (((WordRepetition) pointer1.getElement()).getWord().toLowerCase().equals(wordArray[i].toLowerCase())) {
+                            int oldValue = ((WordRepetition) pointer1.getElement()).getRepetition();
+                            ((WordRepetition) pointer1.getElement()).setRepetition(oldValue + 1);
+                        }
+                        pointer1 = pointer1.getNext();
+                    }
+                    
+                } catch (Exception e){
+                    
+                }
+            }
 
+            for (int i = 0; i < keyWordsArray.length; i++) {
+                int indexHT = FunctionsUI.hashStringUpperC(keyWordsArray[i], wordsHT);
+                
+                Node pointer1 = wordsHT.getTable()[indexHT].getHead();
+                while (pointer1 != null) {
+                    if (((WordRepetition) pointer1.getElement()).getWord().equals(keyWordsArray[i])) {
+                        String word = ((WordRepetition) pointer1.getElement()).getWord();
+                        int quantity = ((WordRepetition) pointer1.getElement()).getRepetition();
+                        textResult += word + ": " + String.valueOf(quantity) + "\n";
+                    }
+                    pointer1 = pointer1.getNext();
+                }
+            }
+             
+        textArea.setText(textResult);
+            
+        } else {
+            JOptionPane.showMessageDialog(null, "Elija uno de los articulos");
+        }
+    }
+    
+    //FALTA JAVADOC
+    public static int hashStringUpperC (String string, HashTable HT) {
+        String str = string.toLowerCase();
+        long hash = 5381;
+        for (int i = 0; i < str.length(); i++) {
+            // Multiply hash by 33 done by bit shift
+            hash = (hash << 5) + hash;
+            // add to hash ascii value of current char
+            hash += (int) str.charAt(i);
+        }
+        // converts the hash to index
+        int index = (int) (hash % HT.getSize());
+        return (index < 0) ? index * -1 : index;
+    }
 }
